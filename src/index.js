77ff1c90901aa2ea392747b9ca69f000219fdb8c
KeslieBox <kesliebox@gmail.com>
2021-04-18 04:18:11 -0500

    commit tree 2241ed4968c1b829a05173e73e384765cf560a5c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    renderImages()
+    loadBreeds()
+})
+
+function renderImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(resp => resp.json())
+        .then(results => {
+            results.message.forEach(imageUrl => addImage(imageUrl))
+        })
+}
+
+function addImage(imageUrl) {
+    const imageContainer = document.querySelector("#dog-image-container")
+    let img = document.createElement('img')
+    img.src = imageUrl
+    imageContainer.appendChild(img)
+}
+
+function loadBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(results => {
+        console.log(results.message)
+    })
+}
+
